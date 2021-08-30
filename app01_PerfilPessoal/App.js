ec19bdf41bfef8f1928d5b766ed6b7d8a6956769
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-08-30 01:47:53 -0300

    MeuPerfil concluído :D

--- app01_PerfilPessoal/App.js
+++ app01_PerfilPessoal/App.js
@@ -1,112 +1,142 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
+import React, { Component, useState } from 'react'
+import { 
   ScrollView,
-  StatusBar,
+  View, 
+  Text, 
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  Dimensions, 
+  Button,
+  Image,
+  Linking
+} from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const estilo = StyleSheet.create({
+  phoneScreen: {
+    height: Dimensions.get('window').height,
+    backgroundColor: '#fff',
+    paddingLeft: 15, paddingRight: 15
+  },
+  profileCard: {
+    width: (Dimensions.get('window').width - 30),
+    minHeight: 50,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: 'transparent',
+    borderRadius: 4,
+    textAlign: 'center',
+    marginTop: 15,
+    marginBottom: 5,
+    alignItems: 'center'
+  },
+  profileImg: {
+    width: 230,
+    height: 230,
+    borderRadius: 500,
+    borderWidth: 1,
+    borderColor: '#c7c7c7'
+  },
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+  textsBox: { marginTop: 15 },
+  nome: { fontSize: 32, fontWeight: 'bold' },
+  login: { fontSize: 22, color: '#aeaeae' },
+  bio: { marginTop: 10, fontSize: 16 },
+  titles: { fontSize: 20, fontWeight: 'bold', marginBottom: 10 },
+
+  card: {
+    width: (Dimensions.get('window').width - 30),
+    minHeight: 50,
+    backgroundColor: '#fff',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: '#cecece' /*transparent' */,
+    borderRadius: 4,
+    marginTop: 15
+  },
+  lastcard: {
+    width: (Dimensions.get('window').width - 30),
+    minHeight: 50,
+    backgroundColor: '#fff',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: '#cecece' /*transparent' */,
+    borderRadius: 4,
+    marginTop: 15,
+    marginBottom: 25
+  }
+})
+
+const Profile = (props) => {
+  return(
+    <View style={estilo.profileCard}>
+      <Image 
+        style={estilo.profileImg} 
+        source={{uri: props.url}}
+      />
+      <View style={estilo.textsBox}>
+        <Text style={estilo.nome}>{props.name}</Text>
+        <Text style={estilo.login}>{props.login}</Text>
+        <Text style={estilo.bio}>{props.bio}</Text>
+        <Text 
+          style={{ marginTop: 15, color: '#0071d3', fontSize: 18 }} 
+          onPress={() => Linking.openURL(props.link)}>
+          Veja meu perfil e projetos no Github
+        </Text>
+      </View>
     </View>
-  );
-};
+  )
+}
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const PerfonalData = (props) => {
+  return(
+    <View style={estilo.card}>
+      <Text style={estilo.titles}>Dados Pessoais</Text>
+      <Text><Text style={{ fontWeight: 'bold' }}>Nome:</Text> {props.name}</Text>
+      <Text><Text style={{ fontWeight: 'bold' }}>Idade:</Text> {props.age} de idade</Text>
+      <Text><Text style={{ fontWeight: 'bold' }}>Endereço:</Text> {props.adress}</Text>
+    </View>
+  )
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const Qualification = () => {
+  return(
+    <View style={estilo.card}>
+      <Text style={estilo.titles}>Qualificação Profissional</Text>
+      <Text style={{ fontWeight: 'bold' }}>Graduação em Sistemas para Internet</Text>
+      <Text>Fatec Rubens Lara - 2019 até 2021</Text>
+      <Text style={{ fontWeight: 'bold' }}>Curso Técnico em Informática para Internet</Text>
+      <Text>ETEC Aristóteles Ferreira - 2018 até 2019</Text>
+    </View>
+  )
+}
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+const ProfessionalExperience = () => {
+  return(
+    <View style={estilo.lastcard}>
+      <Text style={estilo.titles}>Experiência Profissional</Text>
+      <Text>Inexperiente pofissionalmente 😅</Text>
+    </View>
+  )
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const Screen = () => {
+  return(
+    <ScrollView style={estilo.phoneScreen}>
+       <Profile 
+        url="https://avatars.githubusercontent.com/u/44307177?v=4"
+        name="Ivan Guimarães"
+        login="2504Guimaraes"
+        bio="Studying in College - To find out how technology works it's rough and funny at the same time."
+        link='https://github.com/2504Guimaraes'
+       />
+       <PerfonalData 
+        name='Ivan Guimarães dos S. da Silva'
+        age='22'
+        adress='Guarujá - SP'
+       />
+       <Qualification/>
+       <ProfessionalExperience/>
+    </ScrollView>
+  )
+}
 
-export default App;
+export default Screen
