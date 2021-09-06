a765f20e8f78608da039a05380dc549ce523c2c6
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-09-06 16:31:47 -0300

    aprendendo a usar pressable :v

--- app05_imc/src/App.js
+++ app05_imc/src/App.js
@@ -5,7 +5,8 @@ import {
   StyleSheet, 
   Dimensions, 
   Image, 
-  TextInput 
+  TextInput, 
+  Pressable
 } from 'react-native'
 
 const estilo = StyleSheet.create({
@@ -13,13 +14,59 @@ const estilo = StyleSheet.create({
     height: Dimensions.get('window').height,
     paddingLeft: 15,
     paddingRight: 15
+  },
+  btn: {
+    backgroundColor: 'lightblue',
+    marginTop: 15,
+    padding: 15,
+    alignItems: 'center'
+  },
+  btnPressed: {
+    backgroundColor: 'pink',
+    marginTop: 15,
+    padding: 15,
+    alignItems: 'center'
   }
 })
+
+class Teste extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = { pressionado: 'Não :(' }
+
+    this.pressionar = this.pressionar.bind(this)
+  }
+
+  pressionar() {
+    this.state.pressionado === 'Não :(' ?
+      this.setState({ pressionado: 'Sim" :P' }) :
+      this.setState({ pressionado: 'Não :(' })
+  }
+
+  render() {
+    return(
+      <View>
+          <Pressable
+            style={ ({pressed}) => [
+              pressed ? estilo.btn : estilo.btnPressed   
+            ]}
+            onPress={ this.pressionar }
+          >
+          <Text>Clique aqui!</Text>
+        </Pressable>
+        <Text>
+          { this.state.pressionado }
+        </Text>
+      </View>
+    )
+  }
+}
 
 export default App = () => {
   return(
     <View style={estilo.screen}>
-      <Text>oie...</Text>
+      <Teste/>
     </View>
   )
 }
