dc092c2d94d896ef94816f26e65cbe3e6535e471
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-09-06 23:08:47 -0300

    App05 completo! :D

--- app05_imc/src/App.js
+++ app05_imc/src/App.js
@@ -8,6 +8,7 @@ import {
   TextInput, 
   Pressable
 } from 'react-native'
+import { set } from 'react-native-reanimated'
 
 const estilo = StyleSheet.create({
   screen: {
@@ -15,48 +16,167 @@ const estilo = StyleSheet.create({
     paddingLeft: 15,
     paddingRight: 15
   },
-  btn: {
-    backgroundColor: 'lightblue',
+  title: {
+    paddingTop: 15,
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+  imgWrapper: {
+    marginTop: 15,
+    alignItems: 'center',
+    alignContent: 'center',
+    padding: 10
+  },
+  imgBox: { 
+    backgroundColor: 'aliceblue', 
+    padding: 15,
+    borderRadius: 100,
+    height: 110,
+    width: 110,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 2, borderStyle: 'dashed'
+  },
+  imcImg: {
+    height: 60, width: 60,
+    backgroundColor: 'transparent',
+  },
+  inputTitles: {
+    marginTop: 8,
+    fontWeight: 'bold'
+  },
+  inputs: {
+    marginTop: 5,
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 3
+  },
+  baseBtn: {
     marginTop: 15,
     padding: 15,
-    alignItems: 'center'
+    alignItems: 'center',
+    borderRadius: 3,
+    borderWidth: 2
+  },
+  btn: {
+    backgroundColor: '#2271ff',
+    borderColor: '#0655e4'
   },
   btnPressed: {
-    backgroundColor: 'pink',
-    marginTop: 15,
-    padding: 15,
-    alignItems: 'center'
+    backgroundColor: '#0d31b2',
+    borderColor: '#002094'
+  },
+  btnTxt: {
+    color: '#fff', 
+    fontWeight: 'bold' 
+  },result: {
+    marginTop: 35,
+    textAlign: 'center'
   }
 })
 
-class Teste extends Component {
+class CalculadoraImc extends Component {
   constructor(props) {
     super(props)
     
-    this.state = { pressionado: 'Não :(' }
+    this.state = { peso: null, altura: null, msg: '' }
 
-    this.pressionar = this.pressionar.bind(this)
+    this.input1 = React.createRef()
+    this.input2 = React.createRef()
+
+    this.pegaPeso = this.pegaPeso.bind(this)
+    this.pegaAltura = this.pegaAltura.bind(this)
+    this.calcularImc = this.calcularImc.bind(this)
+    this.verImc = this.verImc.bind(this)
   }
 
-  pressionar() {
-    this.state.pressionado === 'Não :(' ?
-      this.setState({ pressionado: 'Sim" :P' }) :
-      this.setState({ pressionado: 'Não :(' })
+  pegaPeso(pesoDigitado) { this.setState({ peso: pesoDigitado }) }
+
+  pegaAltura(alturaDigitada) { this.setState({ altura: alturaDigitada }) }
+
+  calcularImc(imc, altura) {
+    if (altura > 0.35 && altura < 2.8) {
+      if (imc >= 40) 
+        this.setState({ msg: 'Obesidade Grau III' })
+      else if (imc >= 35 && imc <= 39.9)
+        this.setState({ msg: 'Obesidade Grau II' })
+      else if (imc >= 30 && imc <= 34.9)
+        this.setState({ msg: 'Obesidade Grau I' })
+      else if (imc >= 25 && imc <= 29.9) 
+        this.setState({ msg: 'Sobrepeso' })
+      else if (imc >= 18.5 && imc <= 24.9) 
+        this.setState({ msg: 'Peso Normal' })
+      else if (imc <= 18.5) 
+        this.setState({ msg: 'Abaixo do peso' })
+    }
+    else {
+      this.setState({ msg: 'Altura inválida!' })
+    }
+  }
+
+  verImc() {
+    const altura = this.state.altura
+    const peso = this.state.peso
+    if (altura != null && peso !== null) {
+      if (!isNaN(altura) && !isNaN(peso)) {
+        const vlImc = (peso / (altura * altura)).toFixed(2)
+        this.calcularImc(vlImc, altura)
+        this.input1.current.clear()
+        this.input2.current.clear()
+        this.setState({ peso: null, altura: null })    
+      }
+      else {
+        this.setState({ msg: 'Digite apenas números!' })
+      }
+    }
+    else {
+      this.setState({ msg: 'Digite em todos os campos!' })
+    }
   }
 
   render() {
     return(
       <View>
-          <Pressable
-            style={ ({pressed}) => [
-              pressed ? estilo.btn : estilo.btnPressed   
-            ]}
-            onPress={ this.pressionar }
-          >
-          <Text>Clique aqui!</Text>
+        <Text style={estilo.title}>
+          Cálculo do Imc
+        </Text>
+        <View style={estilo.imgWrapper}>
+          <View style={estilo.imgBox}>
+            <Image style={estilo.imcImg} 
+              source={require('./images/img_imc.png')}/>
+          </View>
+        </View>
+
+        <Text style={estilo.inputTitles}>Digite seu peso (kg)</Text>
+        
+        <TextInput
+          ref={ this.input1 }
+          onChangeText={ this.pegaPeso }
+          style={estilo.inputs}
+          placeholder=" Ex: 75" />
+
+        <Text style={estilo.inputTitles}>Digite sua altura (m)</Text>
+        
+        <TextInput
+          ref={ this.input2 }
+          onChangeText={ this.pegaAltura }
+          style={estilo.inputs}
+          placeholder=" Ex: 1.78" />
+
+        <Pressable
+          style={ ({pressed}) => [
+            estilo.baseBtn,
+            pressed ? estilo.btnPressed : estilo.btn  
+          ]}
+          onPress={ this.verImc }>
+          <Text style={estilo.btnTxt}>
+            Calcular IMC
+          </Text>
         </Pressable>
-        <Text>
-          { this.state.pressionado }
+
+        <Text style={estilo.result}>
+            { this.state.msg }
         </Text>
       </View>
     )
@@ -66,7 +186,7 @@ class Teste extends Component {
 export default App = () => {
   return(
     <View style={estilo.screen}>
-      <Teste/>
+      <CalculadoraImc/>
     </View>
   )
 }
