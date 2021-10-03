7954e7a4a4cec431eaf018d254296d4c49c0932a
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-10-03 20:17:29 -0300

    App09 concluído :D

--- app09_Vagas/src/App.js
+++ app09_Vagas/src/App.js
@@ -10,29 +10,24 @@ import {
 
 const style = StyleSheet.create({
   app: {
-    backgroundColor: '#eaeded',
-    height: Dimensions.get('window').height,
+    backgroundColor: '#eaeded'
   },
   carrossel: {
-    width: Dimensions.get('window').width,
-    height: 420
+    width: Dimensions.get('window').width
   },
-  produtos: {
+  ofertas: {
     marginTop: 15,
-    height: 390,
-    width: 280,
     backgroundColor: '#fff',
     marginLeft: 15,
-    padding: 15
+    marginRight: 15,
+    padding: 25
   }, 
-  lastProduct: {
-    borderRadius: 3,
+  ultimaVaga: {
     marginTop: 15,
-    height: 390,
-    width: 280,
     backgroundColor: '#fff',
     marginLeft: 15, 
     marginRight: 15,
+    marginBottom: 15,
     padding: 15
   },
   titles: {
@@ -40,9 +35,11 @@ const style = StyleSheet.create({
     fontWeight:'bold',
     color: '#000'
   },
-  imgs: {
-    marginTop: 15,
-    height: 220, width: 250
+  titleRequirements: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#414141'
   },
   textos: {
     marginTop: 30,
@@ -60,33 +57,67 @@ class App extends Component {
     return(
       <View style={style.app}>
         <View style={style.carrossel}>
-          <ScrollView 
-            horizontal={true} 
-            showsHorizontalScrollIndicator={false}>
-            <View style={style.produtos}>
+          <ScrollView>
+
+            <View style={style.ofertas}>
               <Text style={style.titles}>
-                Leia à vontade com Kindle Unlimited
+                Desenvolvedor Python Júnior
               </Text>
-              <Image style={style.imgs}
-                source={require('./images/img01.jpg')}/>
-              <Text style={style.textos}>Veja todas as ofertas</Text>
+              <Text style={style.titleRequirements}>Salário:</Text>
+              <Text>{'\u2022'} R$ 2950.95</Text>
+              <Text style={style.titleRequirements}>
+                O que esperamos:
+              </Text>
+              <Text>• Experiência em programação Python;</Text>
+              <Text>• Versionamento Git;</Text>
+              <Text>• Banco de dados SQL;</Text>
+              <Text>• Perfil comunicativo, ágil, curioso e pró-ativo em resolver problemas.</Text>
+              <Text style={style.titleRequirements}>
+                As principais tecnologias e ferramentas que usamos atualmente:
+              </Text>
+              <Text>• MySQL e MongoDB;</Text>
+              <Text>• Módulos e frameworks Python: spaCy, jupyter, pandas;</Text>
+              <Text>• Cloud (AWS e Google);</Text>
+              <Text>• Gitlab, Slack, Trello.</Text>
+              <Text style={style.textos}>Entre em contato conosco</Text>
             </View>
-            <View style={style.produtos}>
+
+            <View style={style.ofertas}>
               <Text style={style.titles}>
-                Amazon Music Unlimited: 30 dias grátis
+                Desenvolvedor Java Senior
               </Text>
-              <Image style={style.imgs}
-                source={require('./images/img02.jpg')}/>
-              <Text style={style.textos}>Experimente agora</Text>
+              <Text style={style.titleRequirements}>Salário:</Text>
+              <Text>{'\u2022'} R$ 8988.15</Text>
+              <Text style={style.titleRequirements}>Descrição da vaga</Text>
+              <Text>• Atuar no time de desenvolvimento voltado para área de fundos offshore;</Text>
+              <Text>• Atuar com o sistema core das operações da área, integrações API com sistemas satélites, atualizações e correções;</Text>
+              <Text>• Trabalhar em conjunto com outra área de desenvolvimento focada na refatoração e modernização de códigos e tecnologias (microsserviços, versões Java, hospedagem em AWS);</Text>
+              <Text>• Levantamento de requisitos e alinhamento de demandas e necessidades das áreas de negócios do banco</Text>
+              <Text>• Apresentação de novas soluções e melhorias;</Text>
+              <Text>• Gerenciamento de demanda, negociação de prazos e escopos;</Text>
+              <Text>• Metodologia ágil.</Text>
+              <Text style={style.titleRequirements}>Perfil desejado</Text>
+              <Text>• Java 8, microserviços;</Text>
+              <Text>• Experiência e/ou conhecimentos sobre o mercado financeiro e áreas afins, assim como suas ferramentas, tecnologias e plataformas como, por exemplo, Murez, Findur, Calypso.</Text>
+              <Text style={style.textos}>Entre em contato conosco</Text>
             </View>
-            <View style={style.lastProduct}>
+
+            <View style={style.ultimaVaga}>
               <Text style={style.titles}>
-                Frete GRÁTIS no seu primeiro pedido
+                Desenvolvedor React Júnior
               </Text>
-              <Image style={style.imgs}
-                source={require('./images/img03.jpg')}/>
-              <Text style={style.textos}>Saiba mais</Text>
+              <Text style={style.titleRequirements}>Salário:</Text>
+              <Text>{'\u2022'} R$ 2100.00</Text>
+              <Text style={style.titleRequirements}>Perfil desejado</Text>
+              <Text>• Experiência com JavaScript (ES6+)</Text>
+              <Text>• Experiência com TypeScript</Text>
+              <Text>• Experiência com Styled Components</Text>
+              <Text>• Conhecimentos sólidos em React</Text>
+              <Text>• Conhecimentos sólidos em React Native</Text>
+              <Text>• Experiência com Context API, Redux ou MobX</Text>
+              <Text style={style.textos}>Entre em contato conosco</Text>
             </View>
+
           </ScrollView>
         </View>
       </View>
