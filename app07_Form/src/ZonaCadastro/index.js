7b7fc4fe873f809c3b73499d60f4cd9f35fa8db1
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-10-02 22:50:14 -0300

    reconstruindo form app 07

--- app07_Form/src/ZonaCadastro/index.js
+++ app07_Form/src/ZonaCadastro/index.js
@@ -1,16 +1,79 @@
 import React, { Component } from 'react'
-import { Text, View, ScrollView } from 'react-native'
+import { Text, View, ScrollView, TextInput } from 'react-native'
+import { Picker } from '@react-native-picker/picker'
+import Slider from '@react-native-community/slider'
 import styleZonaCadastro from './style'
 import UsuarioCadastrado from './UsuarioCadastrado'
 
 class ZonaCadastro extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      nome: null,
+      idade: null,
+      sexoPessoa: null,
+      escolaridade: null,
+      valorLimite: 0,
+      brasileiro: false,
+      nacionalidade: null
+    }
+    this.pegaNome = this.pegaNome.bind(this)
+    this.pegaIdade = this.pegaIdade.bind(this)
+    this.pegaSexo = this.pegaSexo.bind(this)
+    this.pegaEscolaridade = this.pegaEscolaridade.bind(this)
+    this.pegaLimite = this.pegaLimite.bind(this)
+    this.pegaNacionalidade = this.pegaNacionalidade.bind(this)
+    // this.mostrarDados = this.mostrarDados.bind(this)
   }
+
+  pegaNome(valorDigitado) { this.setState({ nome:  valorDigitado}) }
+  pegaIdade(valorDigitado) { this.setState({ idade:  valorDigitado}) }
+  pegaSexo(valorDigitado) { this.setState({ sexoPessoa:  valorDigitado}) }
+  pegaEscolaridade(valorDigitado) { this.setState({ escolaridade:  valorDigitado}) }
+  pegaLimite(valorDigitado) { this.setState({ valorLimite:  valorDigitado}) }
+  pegaNacionalidade(valorDigitado) { this.setState({ brasileiro:  valorDigitado}) }
+
   render() {
     return(
       <ScrollView style={styleZonaCadastro.zonaCadastro}>
-        <View style={styleZonaCadastro.formulario}></View>
+        <View style={styleZonaCadastro.formulario}>
+          <Text style={styleZonaCadastro.inputTitles}>Nome</Text>
+          <TextInput
+            onChangeText={ this.pegaNome }
+            placeholder=" Digite seu nome"
+            style={styleZonaCadastro.inputs}
+          />
+          <Text style={styleZonaCadastro.inputTitles}>Idade</Text>
+          <TextInput
+            onChangeText={ this.pegaIdade }
+            placeholder=" Digite sua idade"
+            style={styleZonaCadastro.inputs}
+          />
+          <Text style={styleZonaCadastro.inputTitles}>Sexo</Text>
+          <Picker 
+            selectedValue={this.state.sexoPessoa}
+            onValueChange={ (itemValue, itemIndex) => { 
+              this.setState({ sexoPessoa: itemValue }) 
+            }}>
+            <Picker.Item key={1} value={"Masculino"} label="Masculino"/>
+            <Picker.Item key={2} value={"Feminino"} label="Feminino"/>
+            <Picker.Item key={3} value={"Outro"} label="Outro"/>
+          </Picker>
+          <Text style={styleZonaCadastro.inputTitles}>Escolaridade</Text>
+          <Picker
+            selectedValue={this.state.escolaridade}
+            onValueChange={ (itemValue, itemIndex) => {
+              this.setState({ escolaridade: itemValue })
+            }}>
+            <Picker.Item key={1} value={"Ensino fundamental incompleto"} label="Ensino fundamental incompleto"/>
+            <Picker.Item key={2} value={"Ensino fundamental completo"} label="Ensino fundamental completo"/>
+            <Picker.Item key={3} value={"Ensino médio incompleto"} label="Ensino médio incompleto"/>
+            <Picker.Item key={4} value={"Ensino médio completo"} label="Ensino médio completo"/>
+            <Picker.Item key={5} value={"Ensino superior incompleto"} label="Ensino superior incompleto"/>
+            <Picker.Item key={6} value={"Ensino superior completo"} label="Ensino superior completo"/>
+          </Picker>
+          
+        </View>
         <UsuarioCadastrado />
       </ScrollView>
     )
--- app07_Form/src/ZonaCadastro/style.js
+++ app07_Form/src/ZonaCadastro/style.js
@@ -11,7 +11,33 @@ const styleZonaCadastro = StyleSheet.create({
   formulario: {
     marginTop: 10,
     height: 500,
-    backgroundColor: 'pink',
+    backgroundColor: '#ffffff',
+    padding: 15,
+  },
+  inputTitles: {
+    marginTop: 10,
+  },
+  inputs: {
+    marginTop: 5,
+    borderWidth: 1,
+    borderColor: '#cecece',
+    borderRadius: 2
+  },
+  baseBtn: {
+    marginTop: 25,
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 2
+  },
+  btn: {
+    backgroundColor: '#336fca',
+  },
+  btnPressed: {
+    backgroundColor: '#285aa5',
+  },
+  btnTxt: {
+    color: '#fff',
+    fontSize: 16 
   }
 })
 
