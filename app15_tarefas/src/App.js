d694a0f5b4e1df44283e6eadf13101fe561f7c35
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-11-05 18:16:11 -0300

    App15 configurado

---
+++ app15_tarefas/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { 
+  View, 
+  Text, 
+  Switch, 
+  ScrollView, 
+  TextInput 
+} from 'react-native'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { }
+  }
+
+  render() {
+    return(
+      <View>
+        <Text>app 15</Text>
+      </View>
+    )
+  }
+}
