39be61afbeeb9c991a78f1117bc22e51a08d2d4a
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-11-07 00:24:47 -0300

    Fiz classe funcionar, agora testarei com funções devido a maior eficiência

--- app15_tarefas/src/App.js
+++ app15_tarefas/src/App.js
@@ -8,12 +8,46 @@ import {
   Pressable,
   FlatList
 } from 'react-native'
-import ConfigDB_Tarefas from './Classes/ConfigDB_Tarefas'
+import { openDatabase } from 'react-native-sqlite-storage'
 import style from '../src/style'
 
-const renderTarefa = ({ item }) => {
+const getTarefasAntesDeClasseCarregar = (bancoClasse, objClasse) => {
+  const obterListaTarefas = (res, comprimento) => {
+    let listTarefasBD = []
+    for (let i = 0; i < comprimento; i++) {
+      let tarefaBD = res.rows.item(i)
+      listTarefasBD.push({ 
+        id: tarefaBD.id_tarefa, 
+        nome: tarefaBD.nm_tarefa 
+      })
+    }
+    return listTarefasBD
+  }
+
+  bancoClasse.transaction(transacaoDB => {
+    transacaoDB.executeSql(
+      'SELECT * FROM tb_Tarefa ORDER BY id_tarefa',
+      [],
+      (transacaoDB, response) => {
+        console.log('Tarefas do Banco lidas ANTES DE CARREGAR com sucesso!')
+        let comprimento = response.rows.length
+
+        if (comprimento > 0) {
+          const listaTarefas = obterListaTarefas(response, comprimento)
+          console.log(listaTarefas)
+          objClasse.setState({ tarefasComp: listaTarefas })
+        }
+      },
+      error => {
+        console.log(`Erro ao obter tarefas do Banco: "${error.message}"`)
+      }
+    )
+  })
+}
+
+const renderTarefa = (item) => {
   return(
-    <View style={style.tarefa}>
+    <View key={ item.id } style={style.tarefa}>
       <Text style={style.txtTarefa}>{ item.id } - { item.nome }</Text>
       <Pressable style={style.btnApagar}>
         <Text style={style.txtBtnApagar}>Apagar</Text>
@@ -25,14 +59,85 @@ const renderTarefa = ({ item }) => {
 export default class App extends Component {
   constructor(props) {
     super(props)
+    this.db = openDatabase({ name: 'bd_app_15' })
+    this.db.transaction(transacaoDB => {
+      transacaoDB.executeSql(
+        `CREATE TABLE IF NOT EXISTS tb_Tarefa (
+          id_tarefa INTEGER PRIMARY KEY AUTOINCREMENT,
+          nm_tarefa VARCHAR(25)
+        )`,
+        [],
+        (transacaoDB, response) => {
+          console.log('Tabela Tarefas Criada com sucesso!')
+        },
+        error => {
+          console.log(`Erro ao criar tabela: ${error.message}`)
+        }
+      )
+    })
     this.state = { 
-      tarefasComp: null,
+      tarefasComp: [],
       tarefaDigitada: ''
     }
+    this.pegarNmTarefa = this.pegarNmTarefa.bind(this)
+    getTarefasAntesDeClasseCarregar(this.db, this)
   }
 
-  tarefaAserMandadaProBD(txtTyped) {
-    this.setState({ tarefaDigitada: txtTyped })
+  addNewTarefa() {
+    const newTarefa = this.state.tarefaDigitada
+    if (!newTarefa) {
+      alert("Informe alguma tarefa p/ adicionar uma nova no BD!")
+      return false 
+    }
+    this.db.executeSql(
+      'INSERT INTO tb_Tarefa (nm_tarefa) VALUES (?)',
+      [newTarefa],
+      (transacaoDB, response) => {
+        console.log(`Tarefa: "${newTarefa}" adicionada com sucesso!`)
+        this.getTarefas()
+      },
+      error => {
+        console.log(`Erro ao criar tarefa: "${error.message}"`)
+      }
+    )
+  }
+
+  getTarefasDpsDeClasseCarregar() {
+    const obterListaTarefas = (res, comprimento) => {
+      let listTarefasBD = []
+      for (let i = 0; i < comprimento; i++) {
+        let tarefaBD = res.rows.item(i)
+        listTarefasBD.push({ 
+          id: tarefaBD.id_tarefa, 
+          nome: tarefaBD.nm_tarefa 
+        })
+      }
+      return listTarefasBD
+    }
+
+    this.db.transaction(transacaoDB => {
+      transacaoDB.executeSql(
+        'SELECT * FROM tb_Tarefa ORDER BY id_tarefa',
+        [],
+        (transacaoDB, response) => {
+          console.log('Tarefas do Banco lidas com sucesso!')
+          let comprimento = response.rows.length
+
+          if (comprimento > 0) {
+            const listaTarefas = obterListaTarefas(response, comprimento)
+            console.log(listaTarefas)
+            this.setState({ tarefasComp: listaTarefas })
+          }
+        },
+        error => {
+          console.log(`Erro ao obter tarefas do Banco: "${error.message}"`)
+        }
+      )
+    })
+  }
+
+  pegarNmTarefa(nm) {
+    this.setState({ tarefaDigitada: nm })
   }
 
   render() {
@@ -41,13 +146,15 @@ export default class App extends Component {
       <Text style={style.title}>Aplicativo Tarefas</Text>
       <View style={style.boxBtnInput}>
         <TextInput 
+          onChangeText={ this.pegarNmTarefa }
           style={style.input}
-          placeholder=' Digite o nome da tarefa'
-          value={ this.state.tarefaAserCriada }
-          onChangeText={ null } 
+          placeholder=' Digite o nome da tarefa' 
         />
         <Pressable 
-          onPress= { null }
+          onPress= { () => {
+            this.addNewTarefa()
+            this.getTarefasDpsDeClasseCarregar()
+          }}
           style={
             ({ pressed }) => [
               pressed ? style.btn2 : style.btn
@@ -64,6 +171,15 @@ export default class App extends Component {
         renderItem={ null }
         key={ task => task.id }
       /> */}
+
+      <ScrollView style={style.areaTarefas}>
+        {
+          this.state.tarefasComp.map(tarefaDisponivel => (
+            renderTarefa(tarefaDisponivel)
+          ))
+        }
+        <Text>{ this.state.tarefaDigitada }</Text>
+      </ScrollView>
 
     </View>
     )
--- app15_tarefas/src/Classes/ConfigDB_Tarefas.js
+++ app15_tarefas/src/Classes/ConfigDB_Tarefas.js
@@ -1,77 +1,75 @@
 import { openDatabase } from 'react-native-sqlite-storage'
 
-export default class ConfigDB_Tarefas {
-  constructor() {
-    this.db = openDatabase({ name: 'banco_App_15' })
-  }
-
-  criarTabelas() {
-    this.db.transaction(transacaoDB => {
-      transacaoDB.executeSql(
-        `CREATE TABLE IF NOT EXISTS tb_Tarefa (
-          id_tarefa INTEGER PRIMARY KEY AUTOINCREMENT,
-          nm_tarefa VARCHAR(25)
-        )`,
-        [],
-        (transacaoDB, response) => {
-          console.log('Tabela Tarefas Criada com sucesso!')
-        },
-        error => {
-          console.log(`Erro ao criar tabela: ${error.message}`)
-        }
-      )
-    })
-  }
-
-  addNewTarefa(newTarefa) {
-    if (!newTarefa) {
-      alert("Informe alguma tarefa p/ adicionar uma nova no BD!")
-      return false 
-    }
-    this.db.executeSql(
-      'INSERT INTO tb_Tarefa (nm_tarefa) VALUES (?)',
-      [newTarefa],
+export default createDatabase = (bancoClasse) => {
+  bancoClasse.transaction(transacaoDB => {
+    transacaoDB.executeSql(
+      `CREATE TABLE IF NOT EXISTS tb_Tarefa (
+        id_tarefa INTEGER PRIMARY KEY AUTOINCREMENT,
+        nm_tarefa VARCHAR(25)
+      )`,
+      [],
       (transacaoDB, response) => {
-        console.log(`Tarefa: "${newTarefa}" adicionada com sucesso!`)
-        this.getTarefas()
+        console.log('Tabela Tarefas Criada com sucesso!')
       },
       error => {
-        console.log(`Erro ao criar tarefa: "${error.message}"`)
+        console.log(`Erro ao criar tabela: ${error.message}`)
       }
     )
-  }
+  })
+}
 
-  getTarefas() {
-    const obterListaTarefas = (res, comprimento) => {
-      let listTarefasBD = []
-      for (let i = 0; i < comprimento; i++) {
-        let tarefaBD = res.rows.item(i)
-        listTarefasBD.push({ 
-          id: tarefaBD.id_tarefa, 
-          nome: tarefaBD.nm_tarefa 
-        })
-      }
-      return listTarefasBD
+export const getTarefas = (bancoClasse, objClasse) => {
+  const obterListaTarefas = (res, comprimento) => {
+    let listTarefasBD = []
+    for (let i = 0; i < comprimento; i++) {
+      let tarefaBD = res.rows.item(i)
+      listTarefasBD.push({ 
+        id: tarefaBD.id_tarefa, 
+        nome: tarefaBD.nm_tarefa 
+      })
     }
-
-    this.db.transaction(transacaoDB => {
-      transacaoDB.executeSql(
-        'SELECT * FROM tb_Tarefa ORDER BY id_tarefa DESC',
-        [],
-        (transacaoDB, response) => {
-          console.log('Tarefas do Banco lidas com sucesso!')
-          let comprimento = response.rows.length
-
-          if (comprimento > 0) {
-            const listaTarefas = obterListaTarefas(response, comprimento)
-            console.log(listaTarefas)
-            return listaTarefas
-          }
-        },
-        error => {
-          console.log(`Erro ao obter tarefas do Banco: "${error.message}"`)
-        }
-      )
-    })
+    return listTarefasBD
   }
+
+  bancoClasse.transaction(transacaoDB => {
+    transacaoDB.executeSql(
+      'SELECT * FROM tb_Tarefa ORDER BY id_tarefa',
+      [],
+      (transacaoDB, response) => {
+        console.log('Tarefas do Banco lidas ANTES DE CARREGAR com sucesso!')
+        let comprimento = response.rows.length
+
+        if (comprimento > 0) {
+          const listaTarefas = obterListaTarefas(response, comprimento)
+          console.log(listaTarefas)
+          objClasse.setState({ tarefasComp: listaTarefas })
+        }
+      },
+      error => {
+        console.log(`Erro ao obter tarefas do Banco: "${error.message}"`)
+      }
+    )
+  })
+}
+
+export const addNewTarefa = (
+  bancoClasse, 
+  objClasse, 
+  newTarefa
+) => {
+  if (!newTarefa) {
+    alert("Informe alguma tarefa p/ adicionar uma nova no BD!")
+    return false 
+  }
+  bancoClasse.executeSql(
+    'INSERT INTO tb_Tarefa (nm_tarefa) VALUES (?)',
+    [newTarefa],
+    (transacaoDB, response) => {
+      console.log(`Tarefa: "${newTarefa}" adicionada com sucesso!`)
+      getTarefas(bancoClasse, objClasse)
+    },
+    error => {
+      console.log(`Erro ao criar tarefa: "${error.message}"`)
+    }
+  )
 }
