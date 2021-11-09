2fc10e57c0736d0ffa80c31372576afd22196302
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-11-09 17:30:13 -0300

    App 15 concluído! :D
    Tive que ultilizar funções e Hooks p/ garantir
    funcionamento assíncrono. Também percebi que meus
    métodos executeSql estavam errados na sua indexação!

--- app15_tarefas/src/App.js
+++ app15_tarefas/src/App.js
@@ -1,72 +1,131 @@
-import React, { Component, useEffect } from 'react'
-import { 
-  View, 
-  Text, 
-  Switch, 
-  ScrollView, 
-  TextInput, 
-  Pressable,
-  FlatList
-} from 'react-native'
-
+import React, { Component, useEffect, useState } from 'react'
+import { View, Text, Switch, ScrollView, TextInput, Pressable,FlatList } from 'react-native'
 import { openDatabase } from 'react-native-sqlite-storage'
-import criarTabelasDB, { 
-  getTarefas, 
-  addNewTarefa,
-  deleteTarefa 
-} from '../src/Functions/ConfigDB_Tarefas'
 import style from '../src/style'
-
-
-const renderTarefa = (task, bancoClasse, objClasse) => {
-  return(
-    <View key={ task.id } style={style.tarefa}>
-      <Text style={style.txtTarefa}>{ task.id } - { task.nome }</Text>
-      <Pressable style={style.btnApagar}
-        onPress={ () => { 
-          deleteTarefa(bancoClasse, objClasse, task.id) 
-        }}
-      >
-        <Text style={style.txtBtnApagar}>Apagar</Text>
-      </Pressable>
-    </View>
-  )
-}
 
 const myDB = openDatabase({ name: 'bancoApp15' })
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.db = myDB 
-    criarTabelasDB(this.db)
-    this.state = { 
-      tarefasComp: [],
-      tarefaDigitada: ''
+export default App = () => {
+  const [tarefa, setTarefa] = useState('')
+  const [tarefas, setTarefas] = useState([])
+
+  const msgConsole = (txt) => {
+    console.log('--------------------------------------------------')
+    console.log(`- ${txt} -`)
+    console.log('--------------------------------------------------')
+  }
+
+  const criarTabelas = () => {
+    myDB.transaction(txn => { 
+      txn.executeSql(
+        `CREATE TABLE IF NOT EXISTS tb_tarefas (
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          nome VARCHAR(20)
+        )`,
+        [],
+        (sqlTxn, response) => {
+          msgConsole('Tabela do Banco criada com sucesso')
+        },
+        error => {
+          msgConsole(`Erro ao criar tabela: ${error.message}`)
+        }
+      )
+    })
+  }
+
+  const deleteTarefa = (id) => {
+    myDB.transaction(txn => {
+      txn.executeSql(
+        `DELETE FROM tb_tarefas WHERE id = ?`, [id],
+        (sqlTxn, response) => {
+          msgConsole(`Tarefa com ID = ${id} deletada com Sucesso!`)
+          setTarefas('')
+          getTarefas()
+        },
+        error => {
+          msgConsole(`Erro ao deletar tarefa com ID = ${id}: ${error.message}`)
+        }
+      )
+    })
+  }
+
+  const addTarefa = () => {
+    if(!tarefa) {
+      alert('Informe alguma tarefa!')
+      return false
     }
-    this.pegarNmTarefa = this.pegarNmTarefa.bind(this)
-    getTarefas(this.db, this)
+
+    myDB.transaction(txn => {
+      txn.executeSql(
+        `INSERT INTO tb_tarefas (nome) VALUES (?)`,
+        [tarefa],
+        (sqlTxn, response) => {
+          msgConsole(`Tarefa "${tarefa}" adicionada com sucesso!`)
+          getTarefas()
+          setTarefa('')
+        },
+        error => {
+          msgConsole(`Erro ao inserir tarefa. Erro: ${error.message}`)
+        }
+      )
+    })
   }
 
-  pegarNmTarefa(nm) {
-    this.setState({ tarefaDigitada: nm })
+  const getTarefas = () => {
+    myDB.transaction(txn => {
+      txn.executeSql(
+        `SELECT * FROM tb_tarefas ORDER BY id`,
+        [],
+        (sqlTxn, response) => {
+          msgConsole('Tarefas do Banco lidas com sucesso!')
+          const len = response.rows.length
+
+          if (len > 0) {
+            const tarefasLidas = []
+            for (let i = 0; i < len; i++) {
+              const tarefa = response.rows.item(i)
+              tarefasLidas.push({ id: tarefa.id, nome: tarefa.nome })
+            }
+            setTarefas(tarefasLidas)
+          }
+        },
+        error => {
+          msgConsole(`Erro ao obter tarefas para o component: ${error.message}`)
+        }
+      )
+    })
   }
 
-  render() {
+  const renderTarefa = ({ item }) => {
     return(
-      <View style={style.phoneScreen}>
+      <View style={style.tarefa}>
+        <Text style={style.txtTarefa}>{ item.id } - { item.nome }</Text>
+        <Pressable style={style.btnApagar}
+          onPress={ () => { deleteTarefa(item.id) }}
+        >
+          <Text style={style.txtBtnApagar}>Apagar</Text>
+        </Pressable>
+      </View>
+    )
+  }
+
+  useEffect(async () => {
+    await criarTabelas()
+    await getTarefas()
+  }, [])
+  
+  return(
+    <View style={style.phoneScreen}>
       <Text style={style.title}>Aplicativo Tarefas</Text>
       <View style={style.boxBtnInput}>
         <TextInput
-          onChangeText={ this.pegarNmTarefa }
+          value={ tarefa }
+          onChangeText={ setTarefa }
           style={style.input}
           placeholder=' Digite o nome da tarefa'
         />
         <Pressable 
-          onPress= { () => {
-            addNewTarefa(this.db, this, this.state.tarefaDigitada)
-            getTarefas(this.db, this)
-          }}
+          onPress={ addTarefa }
           style={
             ({ pressed }) => [
               pressed ? style.btn2 : style.btn
@@ -76,15 +135,15 @@ export default class App extends Component {
         </Pressable>
       </View>
 
-      <ScrollView style={style.areaTarefas}>
-        {
-          this.state.tarefasComp.map(tarefa => (
-            renderTarefa(tarefa, this.db, this)
-          ))
-        }
-      </ScrollView>
+
+      <FlatList 
+        scrollEnabled
+        style={style.areaTarefas}
+        data={ tarefas }
+        renderItem={ renderTarefa }
+        key={ tarefaLida => tarefaLida.id }
+      />
 
     </View>
-    )
-  }
+  )
 }
