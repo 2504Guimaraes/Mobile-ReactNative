0663fb8bd52953c3dc4543ce78a723ff3618ffce
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-11-11 20:56:34 -0300

    App 17 concluído! :D

--- app17_cep/src/App.js
+++ app17_cep/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Text, View, TextInput, Pressable } from 'react-native'
 import axios from 'axios'
-import meuCEP from '../src/meuCep'
 import style from '../src/style'
 
 import CepUsuario from '../src/Components/CepUsuario/CepUsuario'
@@ -20,7 +19,73 @@ export default class App extends Component {
       loading: true,
       cepComponent: {}
     }
+    this.pegarCep = this.pegarCep.bind(this)
+    this.buscarCep = this.buscarCep.bind(this)
+    this.renderCep = this.renderCep.bind(this)
   }
+
+  async componentDidUpdate() {
+    const showMsg = (txt) => {
+      console.log('--------------------------------------------------')
+      console.log(`${txt}`)
+      console.log('--------------------------------------------------')
+    }
+
+    if (this.state.buscaIniciada === false)
+      return null 
+    else {
+      try {
+        const response = await apiCEP.get(`${this.state.cepBuscado}/json/`)
+        this.setState({
+          loading: false,
+          cepComponent: response.data
+        })
+      } catch(error) {
+        showMsg(error.message)
+      }
+    }
+  }
+
+  renderCep() {
+    if (this.state.buscaIniciada != true) {
+      return(
+        <View>
+          <Text>
+            Digite um CEP para buscarmos informações
+            a respeito do mesmo.
+          </Text>
+        </View>
+      )
+    } else {
+      if(this.state.loading == true) {
+        return(
+          <View>
+            <Text>Carregando dados do CEP...</Text>
+          </View>
+        )
+      } else {
+        return(
+          <CepUsuario 
+              data={{ 
+                cep: this.state.cepComponent.cep,
+                logradouro: this.state.cepComponent.logradouro,
+                bairro: this.state.cepComponent.bairro,
+                localidade: this.state.cepComponent.localidade,
+                uf: this.state.cepComponent.uf
+              }} 
+          />
+        )
+      }
+    }
+  }
+
+  buscarCep() {
+    this.setState({ cepBuscado: this.state.cepDigitado })
+    this.setState({ cepDigitado: '' })
+    this.setState({ buscaIniciada: true })
+  }
+
+  pegarCep(txt) { this.setState({ cepDigitado: txt }) }
 
   render() {
     return(
@@ -28,60 +93,23 @@ export default class App extends Component {
         <Text style={style.title}>Cep X Endereço</Text>
         <View style={style.inputBox}>
             <TextInput style={style.input}
+              value={ this.state.cepDigitado }
               placeholder=' Digite o CEP'
+              onChangeText={ this.pegarCep }
             />
-            <Pressable style={
-              ({ pressed }) => [
-                pressed ? style.btnAtivado : style.btnDesativado
-              ]
-            }>
+            <Pressable 
+              onPress={ this.buscarCep }
+              style={
+                ({ pressed }) => [
+                  pressed ? style.btnAtivado : style.btnDesativado
+              ]}
+            >
               <Text style={style.btnTxt}>Enviar</Text>
             </Pressable>
         </View>
+
+        <View>{ this.renderCep() }</View>
       </View>
     )
   }
 }
-
-// export default class App extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       cepComponent: {},
-//       loading: true
-//     }
-//   }
-
-//   async componentDidMount() {
-//     const response = await apiCEP.get(`/${meuCEP}/json`)
-//     this.setState({
-//       cepComponent: response.data,
-//       loading: false
-//     })
-//   }
-
-//   render() {
-//     if(this.state.loading) {
-//       return(
-//         <View style={style.screen}>
-//           <Text>Loading...</Text>
-//         </View>
-//       )
-//     }
-//     else {
-//       return(
-//         <View style={style.screen}>
-//           <CepUsuario 
-//             data={{ 
-//               cep: this.state.cepComponent.cep,
-//               logradouro: this.state.cepComponent.logradouro,
-//               bairro: this.state.cepComponent.bairro,
-//               localidade: this.state.cepComponent.localidade,
-//               uf: this.state.cepComponent.uf
-//             }} 
-//           />
-//         </View>
-//       )
-//     }
-//   }
-// }
