2b40fe24832010c309e72985e7daed47b0fa9e85
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-11-11 20:13:27 -0300

    Avansando proj

--- app17_cep/src/App.js
+++ app17_cep/src/App.js
@@ -14,41 +14,74 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      cepComponent: {},
-      loading: true
+      cepDigitado: '',
+      buscaIniciada: false,
+      cepBuscado: '',
+      loading: true,
+      cepComponent: {}
     }
-  }
-
-  async componentDidMount() {
-    const response = await apiCEP.get(`/${meuCEP}/json`)
-    this.setState({
-      cepComponent: response.data,
-      loading: false
-    })
   }
 
   render() {
-    if(this.state.loading) {
-      return(
-        <View style={style.screen}>
-          <Text>Loading...</Text>
+    return(
+      <View style={style.screen}>
+        <Text style={style.title}>Cep X Endereço</Text>
+        <View style={style.inputBox}>
+            <TextInput style={style.input}
+              placeholder=' Digite o CEP'
+            />
+            <Pressable style={
+              ({ pressed }) => [
+                pressed ? style.btnAtivado : style.btnDesativado
+              ]
+            }>
+              <Text style={style.btnTxt}>Enviar</Text>
+            </Pressable>
         </View>
-      )
-    }
-    else {
-      return(
-        <View style={style.screen}>
-          <CepUsuario 
-            data={{ 
-              cep: this.state.cepComponent.cep,
-              logradouro: this.state.cepComponent.logradouro,
-              bairro: this.state.cepComponent.bairro,
-              localidade: this.state.cepComponent.localidade,
-              uf: this.state.cepComponent.uf
-            }} 
-          />
-        </View>
-      )
-    }
+      </View>
+    )
   }
 }
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       cepComponent: {},
+//       loading: true
+//     }
+//   }
+
+//   async componentDidMount() {
+//     const response = await apiCEP.get(`/${meuCEP}/json`)
+//     this.setState({
+//       cepComponent: response.data,
+//       loading: false
+//     })
+//   }
+
+//   render() {
+//     if(this.state.loading) {
+//       return(
+//         <View style={style.screen}>
+//           <Text>Loading...</Text>
+//         </View>
+//       )
+//     }
+//     else {
+//       return(
+//         <View style={style.screen}>
+//           <CepUsuario 
+//             data={{ 
+//               cep: this.state.cepComponent.cep,
+//               logradouro: this.state.cepComponent.logradouro,
+//               bairro: this.state.cepComponent.bairro,
+//               localidade: this.state.cepComponent.localidade,
+//               uf: this.state.cepComponent.uf
+//             }} 
+//           />
+//         </View>
+//       )
+//     }
+//   }
+// }
