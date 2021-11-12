4d32eebe6b327fa515dc14a5346882c8c2ae0d5c
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-11-12 17:58:15 -0300

    Library p/ picker adicionado

---
+++ app19_conversor/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { View, Text, TextInput } from 'react-native'
+import axios from 'axios'
+import { Picker } from '@react-native-picker/picker'
+import style from '../src/style'
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <View>
+        <Text>oie</Text>
+      </View>
+    )
+  }
+}
