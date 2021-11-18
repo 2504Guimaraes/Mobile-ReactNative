b4a16827e7e16b1661c708761a47122321ad935e
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-11-18 18:40:41 -0300

    React navigation instalado

--- app22_contatos/android/app/src/main/java/com/app22_contatos/MainActivity.java
+++ app22_contatos/android/app/src/main/java/com/app22_contatos/MainActivity.java
@@ -1,6 +1,7 @@
 package com.app22_contatos;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "app22_contatos";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
