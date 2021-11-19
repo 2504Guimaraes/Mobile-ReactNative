59a96e72558469c25fef600248f309547d86e15a
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-11-18 23:50:20 -0300

    Axios instalado e Stack semi configurado

--- app22_contatos/src/App.js
+++ app22_contatos/src/App.js
@@ -2,7 +2,10 @@ import 'react-native-gesture-handler'
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
-const App = () => {
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+export default App = () => {
   return(
     <View>
       <Text>Teste</Text>
