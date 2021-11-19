ee9fc1a80f65fed7a869753bd33ad853f02a5685
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-11-19 03:13:01 -0300

    App 22 concluído! :D

--- app22_contatos/src/App.js
+++ app22_contatos/src/App.js
@@ -5,10 +5,50 @@ import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
+import Contatos from './Components/Contatos'
+import Form from './Components/Form'
+
+const Stack = createStackNavigator()
+
+const infoHeaderTelas = (a, b, c, d = false) => {
+  return {
+    title: `${a}`,
+    headerTitleAlign: 'center',
+    headerTitleStyle: { fontSize: 23, fontWeight: 'bold' },
+    headerStyle: { 
+      backgroundColor: `${b}`, 
+      elevation: 0
+    },
+    headerTintColor: `${c}`,
+    headerShown: d
+  }
+}
+
 export default App = () => {
   return(
-    <View>
-      <Text>Teste</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen 
+          name="Contatos"  // Contatos lidos pela minha API
+          component={ Contatos } 
+          options={ infoHeaderTelas(
+            'Contatos lidos pela API',
+            'transparent',
+            '#000',
+            false
+          )}
+        />
+        <Stack.Screen 
+          name="Formulario"  // Formulário de criação e edição de contatos
+          component={ Form } 
+          options={ infoHeaderTelas(
+            'Editar / Criar Contato',
+            'transparent',
+            '#000',
+            true
+          )}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
